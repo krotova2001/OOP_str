81d969cb1f567c525f0f093898f1eb0b3d26bb78
Кротов Георгий <zvuk24@gmail.com>
2022-03-27 18:28:05 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "MyStr.h"
+
+int main()
+{
+
+	MyStr a{"Hello"};
+	cout << "\na = " << a;
+	char str2[]{ " World" };
+	MyStr b{ str2 };
+	
+	cout << "\nb = " << b;
+	a = a + b;
+	cout << "\na = " << a;
+	a = a + b;
+	cout << "\na = " << a;
+	a = a + 7;
+	a = a + 7.75;
+	cout << "\na = " << a;
+}
---
+++ MyStr.h
@@ -0,0 +1,113 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class MyStr
+{
+	int len;
+	char* str;
+public:
+	MyStr(): len{0}, str{nullptr}{}
+	explicit MyStr(const char* n) :MyStr()
+	{
+		if (n != nullptr)
+		{
+			len = strlen(n);
+			str = new char[len + 1];
+			strcpy_s(str, len + 1, n);
+		}
+	}
+
+	MyStr(const MyStr& c):MyStr(c.str)
+	{
+	}
+
+	MyStr operator=(MyStr n)
+	{
+		if (str != nullptr)
+		{
+			delete[]str;
+			str = nullptr;
+			len = 0;
+		}
+		if (n.str != nullptr)
+		{
+			len = n.len;
+			str = new char[len + 1];
+			strcpy_s(str, len + 1, n.str);
+		}
+		return *this;
+	}
+	
+	MyStr operator+(MyStr b)
+	{
+		MyStr c;
+		c.len = len + b.len;
+		c.str = new char[c.len + 1];
+		c.str[0] = '\0';
+		if (str != nullptr)
+		{
+			strcat_s(c.str, c.len + 1, str);
+		}
+		if (b.str != nullptr)
+		{
+			strcat_s(c.str, c.len + 1, b.str);
+		}
+		return c;
+	}
+
+	MyStr operator+(int a)
+	{
+		char m[129];
+		snprintf(m, 129, "% i", a);
+		MyStr c;
+		c.len = len + strlen(m);
+		c.str = new char[c.len + 1];
+		c.str[0] = '\0';
+		if (str != nullptr)
+		{
+			strcat_s(c.str, c.len + 1, str);
+		}
+		strcat_s(c.str, c.len + 1, m);
+		return c;
+	}
+
+	MyStr operator+(double a)
+	{
+		char m[129];
+		snprintf(m, 129, "% f", a);
+		MyStr c;
+		c.len = len + strlen(m);
+		c.str = new char[c.len + 1];
+		c.str[0] = '\0';
+		if (str != nullptr)
+		{
+			strcat_s(c.str, c.len + 1, str);
+		}
+		strcat_s(c.str, c.len + 1, m);
+		return c;
+	}
+
+	friend ostream& operator<<(ostream& s, MyStr a)
+	{
+		if (a.len == 0)
+		{
+			s << "Empty";
+		}
+		else
+		{
+			s << a.str;
+		}
+		return s;
+	}
+
+	~MyStr()
+	{
+		if (str != nullptr)
+		{
+			delete[]str;
+		}
+	}
+
+};
+
